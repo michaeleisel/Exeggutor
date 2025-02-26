4cc53ccbba8d38a681b48681679aec37d046076e
Michael Eisel <michael.eisel@gmail.com>
2025-02-25 22:34:27 -0500

    add nonblocking mode

--- lib/exeggutor.rb
+++ lib/exeggutor.rb
@@ -2,6 +2,99 @@ require 'open3'
 require 'shellwords'
 
 module Exeggutor
+  # Represents an in-progress process execution.
+  #
+  # @attr_reader stdout [String] The standard output of the process.
+  # @attr_reader stderr [String] The standard error of the process.
+  # @attr_reader exit_code [Integer] The exit code of the process.
+  class ProcessHandle
+    attr_accessor :stdin_stream
+
+    # @private
+    def initialize(args, env: nil, chdir: nil, stdin: nil)
+      # TODO: expand "~"? popen3 doesn't expand it by default
+      @stdin_stream, @stdout_stream, @stderr_stream, @wait_thread = Exeggutor::run_popen3(args, env, chdir)
+
+      # Make the streams as synchronous as possible, to minimize the possibility of a surprising lack
+      # of output
+      @stdout_stream.sync = true
+      @stderr_stream.sync = true
+
+      @stdout_str = +''  # Using unfrozen strings
+      @stderr_str = +''
+
+      @stdout_subscribers = []
+      @stderr_subscribers = []
+
+      @stdout_mutex = Mutex.new
+      @stderr_mutex = Mutex.new
+
+      @stdout_thread = Thread.new do
+        while (line = @stdout_stream.gets)
+          @stdout_mutex.synchronize do
+            @stdout_str << line
+            for subscriber in @stdout_subscribers
+              subscriber.call(line.dup)
+            end
+          end
+        end
+      end
+
+      @stderr_thread = Thread.new do
+        while (line = @stderr_stream.gets)
+          @stderr_mutex.synchronize do
+            @stderr_str << line
+            for subscriber in @stderr_subscribers
+              subscriber.call(line.dup)
+            end
+          end
+        end
+      end
+    end
+
+    def on_stdout(&block)
+      @stdout_mutex.synchronize do
+        if @stdout_str.size > 0
+          yield(@stdout_str.dup)
+        end
+        @stdout_subscribers << block
+      end
+    end
+
+    def on_stderr(&block)
+      @stderr_mutex.synchronize do
+        if @stderr_str.size > 0
+          yield(@stderr_str.dup)
+        end
+        @stderr_subscribers << block
+      end
+    end
+
+    def result
+      return if @result
+
+      @stdin_stream.close if !@stdin_stream.closed?
+
+      exit_status = @wait_thread.value
+
+      # Ensure all IO is complete
+      @stdout_thread.join
+      @stderr_thread.join
+
+      # Close open pipes
+      @stdout_stream.close
+      @stderr_stream.close
+
+      @result = ProcessResult.new(
+        stdout: @stdout_str.force_encoding('UTF-8'),
+        stderr: @stderr_str.force_encoding('UTF-8'),
+        exit_code: exit_status.exitstatus
+      )
+
+      @result
+    end
+  end
+
   # Represents the result of a process execution.
   #
   # @attr_reader stdout [String] The standard output of the process.
@@ -39,67 +132,32 @@ module Exeggutor
   end
 
   # @private
-  def self.run_popen3(args, env)
+  def self.run_popen3(args, env, chdir)
     # Use this weird [args[0], args[0]] thing for the case where a command with just one arg is being run
+    opts = {}
+    opts[:chdir] = chdir if chdir
     if env
-      Open3.popen3(env, [args[0], args[0]], *args.drop(1))
+      Open3.popen3(env, [args[0], args[0]], *args.drop(1), opts)
     else
-      Open3.popen3([args[0], args[0]], *args.drop(1))
+      Open3.popen3([args[0], args[0]], *args.drop(1), opts)
     end
   end
 
-  def self.run!(args, can_fail: false, show_stdout: false, show_stderr: false, env: nil, cwd: nil, stdin_data: nil)
-    # TODO: expand "~"? popen3 doesn't expand it by default
-    if cwd
-      stdin_stream, stdout_stream, stderr_stream, wait_thread = Dir.chdir(cwd) { Exeggutor::run_popen3(args, env) }
-    else
-      stdin_stream, stdout_stream, stderr_stream, wait_thread = Exeggutor::run_popen3(args, env)
+  def self.exeg(args, can_fail: false, show_stdout: false, show_stderr: false, env: nil, chdir: nil, stdin: nil)
+    raise "args.size must be >= 1" if args.empty?
+    handle = ProcessHandle.new(args, env: env, chdir: chdir, stdin: stdin)
+    handle.stdin_stream.write(stdin) if stdin
+    handle.stdin_stream.close
+
+    handle.on_stdout do |str|
+      puts str if show_stdout
     end
 
-    stdin_stream.write(stdin_data) if stdin_data
-    stdin_stream.close
-
-    # Make the streams as synchronous as possible, to minimize the possibility of a surprising lack
-    # of output
-    stdout_stream.sync = true
-    stderr_stream.sync = true
-
-    stdout_str = +''  # Using unfrozen string
-    stderr_str = +''
-
-    # Start readers for both stdout and stderr
-    stdout_thread = Thread.new do
-      while (line = stdout_stream.gets)
-
-        stdout_str << line
-        print line if show_stdout
-      end
+    handle.on_stderr do |str|
+      warn str if show_stderr
     end
 
-    stderr_thread = Thread.new do
-      while (line = stderr_stream.gets)
-        stderr_str << line
-        warn line if show_stderr
-      end
-    end
-
-    # Wait for process completion
-    exit_status = wait_thread.value
-
-    # Ensure all IO is complete
-    stdout_thread.join
-    stderr_thread.join
-
-    # Close open pipes
-    stdout_stream.close
-    stderr_stream.close
-
-    result = ProcessResult.new(
-      stdout: stdout_str.force_encoding('UTF-8'),
-      stderr: stderr_str.force_encoding('UTF-8'),
-      exit_code: exit_status.exitstatus
-    )
-
+    result = handle.result
     if !can_fail && !result.success?
       error_str = <<~ERROR_STR
         Command failed: #{args.shelljoin}
@@ -120,14 +178,18 @@ end
 # @param can_fail [Boolean] If false, raises a ProcessError on failure.
 # @param show_stdout [Boolean] If true, prints stdout to the console in real-time.
 # @param show_stderr [Boolean] If true, prints stderr to the console in real-time.
-# @param cwd [String, nil] The working directory to run the command in. If nil, uses the current working directory.
-# @param stdin_data [String, nil] Input data to pass to the command's stdin. If nil, doesn't pass any data to stdin.
+# @param chdir [String, nil] The working directory to run the command in. If nil, uses the current working directory.
+# @param stdin [String, nil] Input data to pass to the command's stdin. If nil, doesn't pass any data to stdin.
 # @param env_vars [Hash{String => String}, nil] A hashmap containing environment variable overrides,
 #        or `nil` if no overrides are desired
 #
 # @return [ProcessResult] An object containing process info such as stdout, stderr, and exit code. Waits for the command to complete to return.
 #
 # @raise [ProcessError] If the command fails and `can_fail` is false.
-def run!(...)
-  Exeggutor::run!(...)
+def exeg(...)
+  Exeggutor::exeg(...)
+end
+
+def exeg_async(...)
+  Exeggutor::ProcessHandle.new(...)
 end
--- test/test.rb
+++ test/test.rb
@@ -3,19 +3,61 @@ require_relative "../lib/exeggutor"
 require 'pry-byebug'
 
 class TestYourGem < Minitest::Test
-  def test_something
-    result = run!(%W[echo hi])
+  def test_various
+    result = exeg(%W[echo hi])
     assert_equal "hi\n", result.stdout
     script = 'warn "this is stderr"; puts "this is stdout"; exit 1'
 
     error = assert_raises(Exeggutor::ProcessError) do
-      run!(%W[ruby -e #{script}])
+      exeg(%W[ruby -e #{script}])
     end
     assert_equal "this is stderr\n", error.result.stderr
     assert_equal "this is stdout\n", error.result.stdout
 
-    result = run!(%W[ruby -e #{script}], can_fail: true)
+    result = exeg(%W[ruby -e #{script}], can_fail: true)
     assert_equal "this is stderr\n", error.result.stderr
     assert_equal "this is stdout\n", error.result.stdout
+
+    result = exeg(%W[cat], stdin: "hi")
+    assert_equal "hi", result.stdout
+
+  end
+
+  def test_async
+    return if ENV["EXEG_SKIP_ASYNC"] == "1"
+
+    handle = exeg_async(%W[ruby -e] + ["puts 'foo' ; sleep 2 ; warn 'bar' ; sleep 2 ; puts 'done'"])
+    sleep 1
+    stdout_calls = 0
+    handle.on_stdout do |str|
+      if stdout_calls == 0
+        assert_equal("foo\n", str)
+      elsif stdout_calls == 1
+        assert_equal("done\n", str)
+      else
+        raise
+      end
+      stdout_calls += 1
+    end
+
+    stderr_calls = 0
+    handle.on_stderr do |str|
+      assert_equal(stderr_calls, 0)
+      assert_equal("bar\n", str)
+      stderr_calls += 1
+    end
+    result = handle.result
+    assert_equal(result.stdout, "foo\ndone\n")
+    assert_equal(result.stderr, "bar\n")
+    assert_equal(result.exit_code, 0)
+  end
+
+  def test_chdir
+    handle = exeg_async(%W[false])
+    assert_equal(handle.result.exit_code, 1)
+
+    Dir.mktmpdir do |dir|
+      assert_equal(exeg(%W[pwd], chdir: dir).stdout, "#{File.realpath(dir)}\n")
+    end
   end
 end
